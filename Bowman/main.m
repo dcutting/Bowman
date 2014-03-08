1e4e4ad3c6f877eeed0b98d16c4acf5d52ae4f2c
Dan Cutting <dcutting@gmail.com>
2014-03-08 00:21:59 +0000

    Initial Commit

---
+++ Bowman/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Bowman
+//
+//  Created by Dan Cutting on 8/03/2014.
+//  Copyright (c) 2014 Yellowbek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
